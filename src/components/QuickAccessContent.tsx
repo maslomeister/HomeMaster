0973e848e56be3cd7135b635f5db3552f420cb6e
Nikita Maslov <maslomeister@gmail.com>
2024-08-17 23:29:32 +0300

    removed

--- src/components/QuickAccessContent.tsx
+++
@@ -1,9 +0,0 @@
-import { VFC, useState } from "react";/**
- * The Quick Access Menu content for TabMaster.
- */
-export const QuickAccessContent: VFC<{}> = ({ }) => {
-
-  return (
-    <div>Testing phase</div>
-  );
-};
